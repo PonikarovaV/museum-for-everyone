22a3e7211e19199727d64859bea887d13ed7caef
Vanda Ponikarova <ponikarova@gmail.com>
2020-01-15 16:38:39 +0300

    Add day counter

--- scripts/index.js
+++ scripts/index.js
@@ -12,4 +12,39 @@
         }
     });
 
+    function dayCounter() {
+        let eventDay = new Date(2020, 03, 18).setHours(0);
+        let today = new Date().setHours(0, 0, 0, 0);
+        let distance = eventDay - today;
+        let days = Math.floor(distance/(1000 * 3600 * 24));
+    
+        return days;
+    }
+
+    function declination(number, titles) {
+        cases = [2, 0, 1, 1, 1, 2];
+        return titles[ (number%100>4 && number%100<20)? 2:cases[(number%10<5)?number%10:5] ];
+    };
+
+    function fillDateLine(days, title) {
+
+        if (days < 0) {
+            document.querySelector('.banner__counter').textContent = 'Событие завершилось, но мы еще встретимся! ;)';
+        }
+
+        if (days === 1) {
+            document.querySelector('.banner__counter').textContent = 'Завтра!';
+        }
+
+        if (days === 0) {
+            document.querySelector('.banner__counter').textContent = 'Сегодня!';
+        }
+
+        if (days > 1) {
+            document.querySelector('.banner__counter').textContent = `${days} ${title(days, [' день', ' дня', ' дней'] )}`;
+        }
+    }
+
+    fillDateLine(dayCounter(), declination);
+
 })();
